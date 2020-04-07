e76f494d74d2466e3e02c806aa1debd6de5580bc
Rex Ye <rxye1991@gmail.com>
2020-04-07 19:57:28 -0400

    addingDeleteTodoFeature

--- src/Components/TodoCard.jsx
+++ src/Components/TodoCard.jsx
@@ -6,8 +6,17 @@ export class TodoCard extends Component {
         return (
 
             <div className = 'todoCard'>
-                <h3>{title}</h3>
-                <p>{description}</p>
+                <div className="container">
+                        <div className="row">
+                            <div className="col-md-6 offset-md-2">
+                                <h3> {title} </h3>
+                                <p> {description} </p>
+                            </div>
+                            <div className="col-md-4">
+                                <button type='button' className='btn btn-danger'> Delete </button>
+                            </div>
+                        </div>
+                </div>
             </div>
         )
     }
